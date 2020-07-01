e2ba07f0e387afa801b798d0e88c313c1dae56d6
unknown <srinivasan.ar@husky.neu.edu>
2020-06-30 21:54:42 -0400

    Adding Final changes

---
+++ client/src/config.ts
@@ -0,0 +1,11 @@
+// TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
+const apiId = '6mcpcp34g7'
+
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
+
+
+export const authConfig = {
+  domain: 'dev-r1ecosq7.us.auth0.com',
+  clientId: 'N4Q1KK7KBRVEbkKs2shtnriURDKXpxr5',
+  callbackUrl: 'http://localhost:3000/callback'
+}
